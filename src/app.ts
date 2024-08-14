46bc603f1dfacb645926c019bfedc228a748dec7
Alejandroox <darmdz23@gmail.com>
2024-08-13 22:51:02 -0400

    firts

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { createBot, createFlow, MemoryDB, createProvider} from '@bot-whatsapp/bot'
+
+import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys'
+const main = async () => {
+
+    const provider = createProvider(BaileysProvider)
+
+    provider.initHttpServer(3005)
+
+    provider.http?.server.post('send-message', handleCtx(async (   bot, req, res) => {
+        // const body = req.body
+        const message = req.body.message
+        console.log(message)
+        await bot.sendMessage('584147056701', message, {})
+        res.end('funciono')
+    }))
+    await createBot ({
+        flow: createFlow([]),
+        database: new MemoryDB(),
+        provider
+    })
+}
+
+main()
