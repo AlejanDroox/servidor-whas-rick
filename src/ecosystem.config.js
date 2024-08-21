92074a869da4584555afa5ec67a5cb803a1d1d07
Alejandroox <darmdz23@gmail.com>
2024-08-21 11:17:27 -0400

    return interprete y max cap ram

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -3,6 +3,10 @@ module.exports = {
         name: "server-whatsapp",
         script: "./app.ts", // Apunta al archivo principal de TypeScript
         watch: false,
+        script: "./app.ts", // Cambia esto si tu archivo principal es .js
+        interpreter: "/usr/bin/node", // Asegúrate de especificar el intérprete
+        watch: false,
+        max_memory_restart: '900M',
         exec_mode: 'cluster',
         instances: 1,
         cron_restart: '59 23 * * *',
