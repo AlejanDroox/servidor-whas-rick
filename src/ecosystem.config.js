ccd0f0d77fbe7d3be38070fc073a8bbce791f65a
Alejandroox <darmdz23@gmail.com>
2024-08-23 15:17:43 -0400

    Update ecosystem.config.js

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -3,9 +3,7 @@ module.exports = {
         name: "server-whatsapp2",
         script: "./app.ts", // Apunta al archivo principal de TypeScript
         watch: false,
-        script: "./app.ts", // Cambia esto si tu archivo principal es .js
         interpreter: "/usr/bin/node", // Asegúrate de especificar el intérprete
-        watch: false,
         max_memory_restart: '900M',
         exec_mode: 'cluster',
         instances: 1,
